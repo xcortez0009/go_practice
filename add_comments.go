d74d0a0ad24cbbb3a6759da75cd3887eada69d51
Ximena <xicoga_12_13@hotmail.com>
2019-11-19 15:14:05 -0600

    more exercises

---
+++ add_comments.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+ /*Comment that will have
+  multiple lines */
+ fmt.Println("Go")
+
+ //single line comment
+ fmt.Println("Lang!")
+}
---
+++ string.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main(){
+  var str1 = "This is a string variable"
+  fmt.Println(str1)
+}
